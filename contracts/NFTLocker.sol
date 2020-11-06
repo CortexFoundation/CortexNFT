0b73af898fb18726a0ca3dc6d309df4d76b47579
SteinsElite <lijunji@cortexlabs.ai>
2020-11-06 15:55:02 +0800

    contracts/NFTLocker.sol: add register in locker and set mapping with contract on ethereum

--- contracts/NFTLocker.sol
+++ contracts/NFTLocker.sol
@@ -4,9 +4,17 @@ import "./CRC4/ICRC4.sol";
 import "./CRC4/ICRC4Receiver.sol";
 
 contract NFTLocker is ICRC4Receiver{
+    // All the CRC4 contract could use NFTLocker to deal with cross-chain asset trading, 
+    // and will create a ERC721 contract on the ethereum associated with original contract on cortex
+    // after completly register success and create contract on the ethereum, will set the map
+    mapping(address => address) cortex2Ethereum; 
+
     bytes4 private constant _CRC4_RECEIVED = 0x150b7a02;
+    bytes4 private constant _INTERFACE_ID_CRC4 = 0x80ac58cd;
     
     event Lock(address indexed from, address indexed nftAddr, uint256 indexed tokenId);
+    event Register(address indexed nftAddr);
+    event RegisterSuccess(address indexed _NFTCortexAddr, address indexed _NFTEthereumAddr);
     
     address public governance;
 
@@ -14,6 +22,18 @@ contract NFTLocker is ICRC4Receiver{
         governance = msg.sender;
     }
 
+    function Register(address _NFTAddr) {
+        // verify the contract is a CRC4 contract.
+        require(ICRC4(_NFTAddr).supportsInterface(_INTERFACE_ID_CRC4), 
+            "this contract address does not implement the CRC4");
+        emit Register(_NFTAddr);
+    }
+
+    function setMapping(address _NFTCortexAddr, address _NFTEthereumAddr) {
+        require(msg.sender == governance, " only governance could setup this mapping");
+        cortex2Ethereum[_NFTCortexAddr] = _NFTEthereumAddr;
+        emit RegisterSuccess(_NFTCortexAddr, _NFTEthereumAddr);
+    }
     function onCRC4Received(
         address operator, 
         address from, 
@@ -25,9 +45,9 @@ contract NFTLocker is ICRC4Receiver{
         return _CRC4_RECEIVED;
     }
 
-    function lockNFT(address _nftAddr, uint256 _tokenId) external {
-        ICRC4(_nftAddr).safeTransferFrom(msg.sender, address(this), _tokenId);
-        emit Lock(msg.sender, _nftAddr, _tokenId);
+    function lockNFT(address _NFTAddr, uint256 _tokenId) external {
+        ICRC4(_NFTAddr).safeTransferFrom(msg.sender, address(this), _tokenId);
+        emit Lock(msg.sender, _NFTAddr, _tokenId);
     }
 
 }
