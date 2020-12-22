061d2403c2c9b34caab770b5e3faf6a98a6a2398
Li-Qing <liqingnz@gmail.com>
2020-12-22 18:03:21 +0800

    implementing upgradeable CRC tokens

---
+++ contracts/CRC4Upgradeable/ICRC3.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @dev Interface of the CRC3 standard, as defined in the
+ * [EIP](https://eips.ethereum.org/EIPS/eip-165).
+ *
+ * Implementers can declare support of contract interfaces, which can then be
+ * queried by others (`CRC3Checker`).
+ *
+ * For an implementation, see `CRC3`.
+ */
+interface ICRC3 {
+    /**
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * [EIP section](https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified)
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30 000 gas.
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
