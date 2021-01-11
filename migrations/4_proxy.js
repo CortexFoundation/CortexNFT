669ffb5dc8f70dceedb0ad9ebcebac6070ece0d9
Li-Qing <liqingnz@gmail.com>
2021-01-11 15:18:28 +0800

    fix migrating proxy contract

--- migrations/4_proxy.js
+++ migrations/4_proxy.js
@@ -1,5 +1,5 @@
 const Art = artifacts.require("CortexArtUpgradeable");
-const Proxy = artifacts.require("UpgradeabilityProxy");
+const Proxy = artifacts.require("AdminUpgradeabilityProxy");
 const CRC = artifacts.require("CRC4FullUpgradeable");
 
 
@@ -9,8 +9,8 @@ module.exports = async function(deployer, network, accounts) {
     // await deployer.deploy(CRC);
 
     // return;
-    await deployer.deploy(Art);
+    // await deployer.deploy(Art);
     // let art = await Art.deployed();
     // console.log(art.address);
-    // await deployer.deploy(Proxy, art.address, "0x");
+    await deployer.deploy(Proxy, "0x14c30d15A3BF2Fd99ebe66B1C60674D9BBF89332", owner, "0x");
 };
