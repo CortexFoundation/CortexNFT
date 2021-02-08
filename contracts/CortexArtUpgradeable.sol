9e0eb6631f2164769bfc11881804682cf24beca0
Li-Qing <liqingnz@gmail.com>
2021-02-08 17:50:11 +0800

    update upgreadeable contract

--- contracts/CortexArtUpgradeable.sol
+++ contracts/CortexArtUpgradeable.sol
@@ -472,13 +472,30 @@ contract CortexArtUpgradeable is Initializable, CRC4FullUpgradeable {
 
 
     // Allows owner of a control token to set an immediate buy price. Set to 0 to reset.
-    function makeBuyPrice(uint256 _tokenId, uint256 _amount) external {
+    function makeBuyPrice(uint256 _tokenId, uint256 _buyPrice) external {
         // check if sender is owner/approved of token        
         require(_isApprovedOrOwner(msg.sender, _tokenId));
         // set the buy price
-        sellingState[_tokenId].buyPrice = _amount;
+        sellingState[_tokenId].buyPrice = _buyPrice;
         // emit event
-        emit BuyPriceSet(_tokenId, _amount);
+        emit BuyPriceSet(_tokenId, _buyPrice);
+    }
+
+
+    function setSellingState(uint256 _tokenId, uint256 _buyPrice, uint256 _startTime, uint256 _endTime, uint256 _reservePrice) external {
+        require(_isApprovedOrOwner(msg.sender, _tokenId), "Not the owner!");
+        require(pendingBids[_tokenId].exists == false, "On sale!");
+        require(sellingState[_tokenId].auctionStartTime > now || sellingState[_tokenId].auctionEndTime < now, "There is an existing auction");
+        require(now + maximumAuctionPreparingTime >= _startTime, "Exceed max preparing time");
+        require(_startTime + maximumAuctionPeriod >= _endTime, "Exceed max auction time!");
+        // set the buy price
+        sellingState[_tokenId].buyPrice = _buyPrice;
+        sellingState[_tokenId].auctionStartTime = _startTime;
+        sellingState[_tokenId].auctionEndTime = _endTime;
+        sellingState[_tokenId].reservePrice = _reservePrice;
+        // emit event
+        emit BuyPriceSet(_tokenId, _buyPrice);
+        emit AuctionCreated(_tokenId, sellingState[_tokenId].auctionStartTime, sellingState[_tokenId].auctionEndTime);
     }
 
 
@@ -590,14 +607,14 @@ contract CortexArtUpgradeable is Initializable, CRC4FullUpgradeable {
     function getTokenOnSale() external view returns(uint256[] memory tokenIds) {
         uint256 tokenCount = 0;
         for(uint256 i = 1; i < expectedTokenSupply; ++i) {
-            if(sellingState[i].buyPrice > 0 || sellingState[i].auctionEndTime > 0) {
+            if(sellingState[i].buyPrice > 0 || sellingState[i].auctionEndTime > now) {
                 ++tokenCount;
             }
         }
         tokenIds = new uint256[](tokenCount);
         tokenCount = 0;
         for(i = 1; i < expectedTokenSupply; ++i) {
-            if(sellingState[i].buyPrice > 0 || sellingState[i].auctionEndTime > 0) {
+            if(sellingState[i].buyPrice > 0 || sellingState[i].auctionEndTime > now) {
                 tokenIds[tokenCount] = i;
                 ++tokenCount;
             }
