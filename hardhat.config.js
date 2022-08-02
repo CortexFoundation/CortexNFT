a4aba3a1c39c037457a57f6995b14bab114807c9
steins <lijunji@cortexlabs.ai>
2022-08-02 17:29:05 +0800

    set up hardhat as dev environment for platform contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,6 @@
-require("@nomiclabs/hardhat-waffle");
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    version: "0.8.7",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000,
-      },
-    },
-  },
+  solidity: "0.8.9",
 };
